2888e8130f9ba38ac774aa79bf5eb28b0efd8075
TelAndr <atelikh@gmail.com>
2026-07-15 00:35:11 +0300

    add Main.java to git

--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,2 +1,14 @@
-package hexlet.code;public class Main {
+package hexlet.code;
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        // создаём экземпляр и стартуем приложение
+        Javalin app = App.getApp();
+        // “для разработки”: просто логируйте больше через конфиг Logback (ниже)
+        log.info("Starting app...");
+        app.start();
+    }
 }
