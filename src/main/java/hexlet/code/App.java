b8fe70bb7be03b33d0875b3b9a2958c4ee0c0112
TelAndr <atelikh@gmail.com>
2026-07-21 00:55:52 +0300

    add createTempleteEngine and config.fileRenderer

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,26 +1,39 @@
 package hexlet.code;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import io.javalin.Javalin;
-
-//import static java.lang.reflect.Array.get;
-
 import io.javalin.http.Context;
-import static io.javalin.config.JavalinConfig.config;
+import io.javalin.rendering.template.JavalinJte;
+import static io.javalin.apibuilder.ApiBuilder.*;
 public class App {
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
     private static Javalin appInstance;
 
     public static Javalin getApp() {
         if (appInstance != null) return appInstance;
 
         appInstance = Javalin.create(config -> {
-            config.app4567Port(7000); // при желании замените/уберите
+            //config.app4567Port(7000); // при желании замените/уберите
+            config.jetty.port = 7000;
             config.enableCorsForAllOrigins();
             config.bundledPlugins.enableDevLogging();
-        }).routes(() -> {
-            get("/", ctx -> ctx.result("Hello World"));
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
+            config.routes.apiBuilder(() -> {
+                get("/", ctx -> ctx.result("Hello World"));
+                get("/health", ctx -> ctx.json(new HealthResponse(true)));
+            });
+        });//.routes(() -> {
+        //    get("/", ctx -> ctx.result("Hello World"));
 
             // пример: healthcheck
-            get("/health", ctx -> ctx.json(new HealthResponse(true)));
-        });
+        //    get("/health", ctx -> ctx.json(new HealthResponse(true)));
+        //});
         //appInstance = Javalin.create(config -> {
         //    config.bundledPlugins.enableDevLogging();
         //});
