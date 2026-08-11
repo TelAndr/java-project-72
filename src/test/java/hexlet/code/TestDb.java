ff40a42ec068f15cd5b1d313aafdf908c85760a8
TelAndr <atelikh@gmail.com>
2026-08-12 00:22:38 +0300

    add TestDb.java to git

--- src/test/java/hexlet/code/TestDb.java
+++ src/test/java/hexlet/code/TestDb.java
@@ -1,2 +1,15 @@
-package hexlet.code;public class TestDb {
+package hexlet.code;
+import org.h2.tools.RunScript;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class TestDb {
+    public static Connection connect() throws Exception {
+        return DriverManager.getConnection(
+                System.getProperty("db.jdbcUrl"),
+                System.getProperty("db.user", "sa"),
+                System.getProperty("db.password", "")
+        );
+    }
 }
